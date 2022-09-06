0557534826b37acf6b1391dc6d1b67b05a5d8a49
David Beauchesne <david.beauchesne@uqar.ca>
2022-09-06 15:55:37 -0400

    add species list for the Atlantic

--- Data/SpeciesList/Get_Species_List.R
+++ Data/SpeciesList/Get_Species_List.R
@@ -5,3 +5,8 @@ save(sp, file = './Data/SpeciesList/SpeciesList.RData')
 # Marine mammamls
 load('../eBiotic/Biotic/BioticData/MarineMammals/Data/Biotic/MarineMammalsSP.RData')
 save(mmSp, file = './Data/SpeciesList/MarineMammalsSP.RData')
+
+# Atlantic
+sp_atl <- read.csv("Data/SpeciesList/species_atlantic.csv")
+save(sp_atl, file = './Data/SpeciesList/SpeciesAtlantic.RData')
+
