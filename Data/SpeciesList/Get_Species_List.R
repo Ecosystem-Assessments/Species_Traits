3062152056636ed466e32d17eee92a8bbe68e453
David Beauchesne <david.beauchesne@uqar.ca>
2022-09-19 14:47:00 -0400

    combine species lists

--- Data/SpeciesList/Get_Species_List.R
+++ Data/SpeciesList/Get_Species_List.R
@@ -1,12 +1,21 @@
 # Not reproducible
-load('../eBiotic/Biotic/BioticData/SpeciesList/Data/SpeciesList.RData')
-save(sp, file = './Data/SpeciesList/SpeciesList.RData')
+load('../../NCEA_StLawrence/eBiotic/Biotic/BioticData/SpeciesList/Data/SpeciesList.RData')
+save(sp, file = './Data/SpeciesList/SpeciesStLawrence.RData')
 
 # Marine mammamls
-load('../eBiotic/Biotic/BioticData/MarineMammals/Data/Biotic/MarineMammalsSP.RData')
+load('../../NCEA_StLawrence/eBiotic/Biotic/BioticData/MarineMammals/Data/Biotic/MarineMammalsSP.RData')
 save(mmSp, file = './Data/SpeciesList/MarineMammalsSP.RData')
 
 # Atlantic
 sp_atl <- read.csv("Data/SpeciesList/species_list_nw_atlantic-893b37e8.csv")
 save(sp_atl, file = './Data/SpeciesList/SpeciesAtlantic.RData')
 
+# Single list 
+sp_atl <- dplyr::select(sp_atl, species = SPEC, Count = Freq)
+mmSp <- dplyr::rename(mmSp, Count = CountRec)
+spList <- dplyr::bind_rows(sp_atl, sp, mmSp) |>
+          dplyr::group_by(species) |>
+          dplyr::summarize(Count = sum(Count)) |>
+          dplyr::arrange(species) |>
+          as.data.frame()
+save(spList, file = './Data/SpeciesList/SpeciesList.RData')
