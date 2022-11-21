42b14c60927f97941df0bb93f547bde24826d68f
Andreanne <abeardsell@hotmail.com>
2022-11-21 16:09:24 -0500

    Add 3 manual entries for Phylum

--- Code/Phylum.R
+++ Code/Phylum.R
@@ -1,17 +1,25 @@
 
 # Load species
 spList <- read.csv('./Data/SpeciesList/species_list_nw_atlantic-893b37e8.csv', sep=",", row.names = NULL)
-head(spList)
+
 # see missing entries
 which(is.na(spList$Phylum), arr.ind=TRUE)
 
 # manual entry for:
-spList[353,]
-spList[487,]
-spList[507,]
+#Palmaria palmata
+spList[353,]$Phylum <- 'Rhodophyta'
+
+#Ulva
+spList[487,]$Phylum <- 'Chlorophyta'
+
+#Zostera marina
+spList[507,]$Phylum <- 'Tracheophyta'
+
 
 phylum <- data.frame(spList$SPEC,spList$Phylum)
 colnames(phylum) <- c("Species","Phylum")
+
+phylum
 
 # Export
 save(phylum, file = './Data/SpeciesTraits/Phylum_AjoutAtlantic.RData')
