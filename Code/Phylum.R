3ad0480a3c836fde37209e5cfd2ebf982c02b1cf
David Beauchesne <david.beauchesne@uqar.ca>
2022-12-12 16:03:30 -0500

    review phylum

--- Code/Phylum.R
+++ Code/Phylum.R
@@ -1,4 +1,21 @@
+# =-=-=-=-=-=-=-=-=-=- ST. LAWRENCE SPECIES -=-=-=-=-=-=-=-=-=-= #
+library(magrittr)
+library(tidyverse)
+library(stringr)
+load('./Data/SpeciesList/SpeciesStLawrence.RData')
+nSp <- nrow(sp)
 
+# load taxonomy
+load('./Data/Taxonomy/Taxonomy.RData')
+
+# =-=-=-=-=-=-=-=-=-=- Species attributes from worms -=-=-=-=-=-=-=-=-=-= #
+taxonomy <- stringr::str_split(taxonomy$taxonomy, pattern = ' | ', simplify = T)
+
+# Extract Phylum per taxa
+phylum_stl <- data.frame(taxa = sp$species, phylum = taxonomy[,3],
+                     stringsAsFactors = F)
+
+# =-=-=-=-=-=-=-=-=-=- Atlantic SPECIES -=-=-=-=-=-=-=-=-=-= #
 # Load species
 spList <- read.csv('./Data/SpeciesList/species_list_nw_atlantic-893b37e8.csv', sep=",", row.names = NULL)
 
@@ -16,10 +33,25 @@ spList[487,]$Phylum <- 'Chlorophyta'
 spList[507,]$Phylum <- 'Tracheophyta'
 
 
-phylum <- data.frame(spList$SPEC,spList$Phylum)
-colnames(phylum) <- c("Species","Phylum")
+phylum_atl <- data.frame(spList$SPEC,spList$Phylum)
+colnames(phylum_atl) <- c("taxa","phylum")
 
-phylum
+# =-=-=-=-=-=-=-=-=-=- Combine SPECIES -=-=-=-=-=-=-=-=-=-= #
+load("Data/SpeciesList/SpeciesList.RData")
+phylum <- dplyr::bind_rows(phylum_stl, phylum_atl) |>
+          unique()
+sp <- dplyr::select(spList, -Count) |>
+      dplyr::left_join(phylum, by = c("species" = "taxa"))
+
+# Spread data
+phylum <- sp %>%
+          mutate(value = 1) %>%
+          spread(phylum, value, fill = 0) %>%
+          select(-species) %>%
+          as.matrix()
+
+rownames(phylum) <- sp$species
 
 # Export
-save(phylum, file = './Data/SpeciesTraits/Phylum_AjoutAtlantic.RData')
+write.csv(phylum, file = './Data/SpeciesTraits/Phylum.csv')
+save(phylum, file = './Data/SpeciesTraits/Phylum.RData')
