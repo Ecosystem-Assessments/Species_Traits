dc57025dc19ee655d0eb35fdbfda4e6d3573891d
David Beauchesne <david.beauchesne@uqar.ca>
2023-01-11 11:50:00 -0500

    update phylum

--- Code/Phylum.R
+++ Code/Phylum.R
@@ -2,55 +2,33 @@
 library(magrittr)
 library(tidyverse)
 library(stringr)
-load('./Data/SpeciesList/SpeciesStLawrence.RData')
-nSp <- nrow(sp)
+load('./Data/SpeciesList/SpeciesList.RData')
+nSp <- nrow(spList)
 
-# load taxonomy
-load('./Data/Taxonomy/Taxonomy.RData')
-
-# =-=-=-=-=-=-=-=-=-=- Species attributes from worms -=-=-=-=-=-=-=-=-=-= #
-taxonomy <- stringr::str_split(taxonomy$taxonomy, pattern = ' | ', simplify = T)
-
-# Extract Phylum per taxa
-phylum_stl <- data.frame(taxa = sp$species, phylum = taxonomy[,3],
-                     stringsAsFactors = F)
-
-# =-=-=-=-=-=-=-=-=-=- Atlantic SPECIES -=-=-=-=-=-=-=-=-=-= #
-# Load species
-spList <- read.csv('./Data/SpeciesList/species_list_nw_atlantic-893b37e8.csv', sep=",", row.names = NULL)
-
-# see missing entries
-which(is.na(spList$Phylum), arr.ind=TRUE)
-
-# manual entry for:
-#Palmaria palmata
-spList[353,]$Phylum <- 'Rhodophyta'
-
-#Ulva
-spList[487,]$Phylum <- 'Chlorophyta'
-
-#Zostera marina
-spList[507,]$Phylum <- 'Tracheophyta'
-
-
-phylum_atl <- data.frame(spList$SPEC,spList$Phylum)
-colnames(phylum_atl) <- c("taxa","phylum")
-
-# =-=-=-=-=-=-=-=-=-=- Combine SPECIES -=-=-=-=-=-=-=-=-=-= #
-load("Data/SpeciesList/SpeciesList.RData")
-phylum <- dplyr::bind_rows(phylum_stl, phylum_atl) |>
-          unique()
-sp <- dplyr::select(spList, -Count) |>
-      dplyr::left_join(phylum, by = c("species" = "taxa"))
+classif <- taxize::classification(spList$aphiaID, db = "worms")
+dat <- lapply(
+  classif, 
+  function(x) {
+    ifelse(
+      "Phylum" %in% x$rank,
+      x$name[x$rank == "Phylum"],
+      x$name[x$rank == "Phylum (Division)"]
+    )
+  }
+)
+phylum <- data.frame(
+  species = spList$species,
+  phylum = unlist(dat)
+)
 
 # Spread data
-phylum <- sp %>%
+phylum <- phylum %>%
           mutate(value = 1) %>%
           spread(phylum, value, fill = 0) %>%
           select(-species) %>%
           as.matrix()
 
-rownames(phylum) <- sp$species
+rownames(phylum) <- spList$species
 
 # Export
 write.csv(phylum, file = './Data/SpeciesTraits/Phylum.csv')
